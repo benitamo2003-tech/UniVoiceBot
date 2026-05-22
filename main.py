57f3bebe15f10796cf233548c1dfb9eb78440f8e
benitamo2003-tech <benitamo2003@gmail.com>
2026-05-22 11:29:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def run_flask():
     port = int(os.environ.get("PORT", 8080))
     app_flask.run(host='0.0.0.0', port=port)
 # ================= CONFIG =================
-TOKEN = "8558196271:AAGsm4xqHnFeT7avPKcOVJvcy5pWrq5ZlN0"
+TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = 7997819976
 CHANNEL_ID = "@UniVoiceHub"
 BOT_USERNAME = "UniEchoFeedbackBot"
